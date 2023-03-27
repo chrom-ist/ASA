d555626cb083bcdf7901da63348bdada4827484a
Kovalenko Alexander <ilondontami@gmail.com>
2023-03-27 13:06:08 +0300

    Изменено содержимое контроллера Controllers/DepartmentController.php

---
+++ Project/Program/app/Controllers/DepartmentController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\Controllers;
+
+use app\Models\Department;
+
+class DepartmentController
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store(Department $department)
+    {
+
+    }
+
+    public function edit(Department $department)
+    {
+
+    }
+
+    public function update(Department $department)
+    {
+
+    }
+
+    public function destroy(Department $department)
+    {
+
+    }
+}
