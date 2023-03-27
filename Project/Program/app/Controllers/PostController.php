c064db51021643d2abc5a4a97e857586b0e74428
Kovalenko Alexander <ilondontami@gmail.com>
2023-03-27 14:02:11 +0300

    Изменено содержание контроллера Controllers/PostController

--- Project/Program/app/Controllers/PostController.php
+++ Project/Program/app/Controllers/PostController.php
@@ -1,1 +1,38 @@
-{\rtf1}
+<?php
+
+namespace app\Controllers;
+
+use app\Models\Post;
+
+class PostController
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store(Post $post)
+    {
+
+    }
+
+    public function edit(Post $post)
+    {
+
+    }
+
+    public function update(Post $post)
+    {
+
+    }
+
+    public function destroy(Post $post)
+    {
+
+    }
+}
