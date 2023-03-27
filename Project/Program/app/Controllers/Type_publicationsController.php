ad5d56cfcb34a0702c0d3c4cafdb8ed8f796c495
Kovalenko Alexander <ilondontami@gmail.com>
2023-03-27 14:50:35 +0300

    Изменено содержимоей файла Controllers/Type_publicationsController.php

---
+++ Project/Program/app/Controllers/Type_publicationsController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\Controllers;
+
+use app\Models\Type_publications;
+
+class Type_publicationsController
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store(Type_publications $type_publications)
+    {
+
+    }
+
+    public function edit(Type_publications $type_publications)
+    {
+
+    }
+
+    public function update(Type_publications $type_publications)
+    {
+
+    }
+
+    public function destroy(Type_publications $type_publications)
+    {
+
+    }
+}
