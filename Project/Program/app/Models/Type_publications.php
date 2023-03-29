45c34b72d97cde947d484358a55ead0404c12d38
LolkaOpka <roman15032002@mail.ru>
2023-03-29 23:41:13 +0300

    Изменен файл Type_publications.php

--- Project/Program/app/Models/Type_publications.php
+++ Project/Program/app/Models/Type_publications.php
@@ -8,7 +8,7 @@ namespace app\Models;
  * 
  * Класс описывает сущность типа публикации.
  * 
- * @author Kovalenko Alexander <ilondontami@gmail.com>
+ * @author Beloshitskiy Roman <ilondontami@gmail.com>
  * @version 1.0.0
  */
 class Type_publications
